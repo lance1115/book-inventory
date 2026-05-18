ca6b2942da139232e0fd5f01fe0d114f0faaf4c4
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-18 19:02:15 +0800

    Clarify Supabase email confirmation

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v6";
+const CACHE_NAME = "book-inventory-cache-v7";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260518f",
-  "./supabase-config.js?v=20260518f",
-  "./app-20260518f.js",
+  "./styles.css?v=20260518g",
+  "./supabase-config.js?v=20260518g",
+  "./app-20260518g.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
