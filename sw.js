91f4a3d406a3119423d63158aea2307fbf3816f0
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-19 22:57:37 +0800

    Optimize ISBN lookup speed and sources

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v19";
+const CACHE_NAME = "book-inventory-cache-v20";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260519l",
+  "./styles.css?v=20260519m",
   "./supabase-config.js?v=20260519a",
-  "./app-20260519l.js",
+  "./app-20260519m.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
