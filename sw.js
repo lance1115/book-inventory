079cc6efbf8988fed9f1a100b9f50236c734acdc
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-20 01:16:12 +0800

    Harden cloud inventory clearing

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v28";
+const CACHE_NAME = "book-inventory-cache-v29";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260520e",
+  "./styles.css?v=20260520f",
   "./supabase-config.js?v=20260519a",
-  "./app-20260520e.js",
+  "./app-20260520f.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
