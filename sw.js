8fac6d08a6897c4a877a0697e16f19c0938fd630
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-19 19:30:40 +0800

    Require confirmation before cloud inventory updates

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v14";
+const CACHE_NAME = "book-inventory-cache-v15";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260519g",
+  "./styles.css?v=20260519h",
   "./supabase-config.js?v=20260519a",
-  "./app-20260519g.js",
+  "./app-20260519h.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
