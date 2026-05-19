fb995d5b2a14d13cafa8d79adb152aab8f12a900
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-20 02:16:07 +0800

    Rename app title

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v36";
+const CACHE_NAME = "book-inventory-cache-v37";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260520m",
+  "./styles.css?v=20260520n",
   "./supabase-config.js?v=20260519a",
-  "./app-20260520m.js",
+  "./app-20260520n.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
