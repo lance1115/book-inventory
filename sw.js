6982936c94559918ef6c5ec2074035152a756433
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-19 19:10:51 +0800

    Add cloud inventory management

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v13";
+const CACHE_NAME = "book-inventory-cache-v14";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260519f",
+  "./styles.css?v=20260519g",
   "./supabase-config.js?v=20260519a",
-  "./app-20260519f.js",
+  "./app-20260519g.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
