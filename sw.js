07d29b9b50c74387b15a379ac8bcadcd0d319cca
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-20 02:10:07 +0800

    Render shelves in synchronized A/B lanes

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v35";
+const CACHE_NAME = "book-inventory-cache-v36";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260520l",
+  "./styles.css?v=20260520m",
   "./supabase-config.js?v=20260519a",
-  "./app-20260520l.js",
+  "./app-20260520m.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
