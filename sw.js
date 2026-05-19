4fa630623549278ba99ac441ca391eb0a79ae0d6
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-20 01:37:37 +0800

    Compact cloud sync panel

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v30";
+const CACHE_NAME = "book-inventory-cache-v31";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260520g",
+  "./styles.css?v=20260520h",
   "./supabase-config.js?v=20260519a",
-  "./app-20260520g.js",
+  "./app-20260520h.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
