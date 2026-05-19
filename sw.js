5c0b84ca7e8d13a4d27246190643a572d077cc29
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-19 19:42:41 +0800

    Auto-start scanner and resume after confirmation

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v15";
+const CACHE_NAME = "book-inventory-cache-v16";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260519h",
+  "./styles.css?v=20260519i",
   "./supabase-config.js?v=20260519a",
-  "./app-20260519h.js",
+  "./app-20260519i.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
