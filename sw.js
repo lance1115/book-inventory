3d22b8eb3c6c660504eb872e0780c0c617881213
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-19 20:53:44 +0800

    Remove audit module

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v17";
+const CACHE_NAME = "book-inventory-cache-v18";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260519j",
+  "./styles.css?v=20260519k",
   "./supabase-config.js?v=20260519a",
-  "./app-20260519j.js",
+  "./app-20260519k.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
