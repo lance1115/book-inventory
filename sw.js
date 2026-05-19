247870967a656790354aee60ab3328ac47f240e7
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-20 00:07:54 +0800

    Expand ISBN lookup sources

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v22";
+const CACHE_NAME = "book-inventory-cache-v23";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260519o",
+  "./styles.css?v=20260519p",
   "./supabase-config.js?v=20260519a",
-  "./app-20260519o.js",
+  "./app-20260519p.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
