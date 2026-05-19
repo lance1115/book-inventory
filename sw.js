decd91c1842a653487045757553594d48f20ee13
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-20 00:38:33 +0800

    Keep scanner panel sticky

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v25";
+const CACHE_NAME = "book-inventory-cache-v26";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260520b",
+  "./styles.css?v=20260520c",
   "./supabase-config.js?v=20260519a",
-  "./app-20260520b.js",
+  "./app-20260520c.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
