d0e7608a13609eb2dd1726497e6139a94613f3a7
密码1234 <mima1234@mima1234deMacBook-Air.local>
2026-05-20 01:57:50 +0800

    Remove photo scan helper

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "book-inventory-cache-v33";
+const CACHE_NAME = "book-inventory-cache-v34";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=20260520j",
+  "./styles.css?v=20260520k",
   "./supabase-config.js?v=20260519a",
-  "./app-20260520j.js",
+  "./app-20260520k.js",
   "./manifest.json?v=20260518",
   "./vendor/zxing-browser.min.js?v=20260518",
   "./icons/icon-192.svg",
